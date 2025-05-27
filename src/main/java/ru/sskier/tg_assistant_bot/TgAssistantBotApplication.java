b368b6c3999a862d5210760bbf5f4168ec3f1ad8
Sportik99 <shchetinin.ase@yandex.ru>
2025-05-27 22:19:25 +0600

    start: create repository by develop tg bot

---
+++ src/main/java/ru/sskier/tg_assistant_bot/TgAssistantBotApplication.java
@@ -0,0 +1,13 @@
+package ru.sskier.tg_assistant_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgAssistantBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgAssistantBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/sskier/tg_assistant_bot/TgAssistantBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sskier.tg_assistant_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgAssistantBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
